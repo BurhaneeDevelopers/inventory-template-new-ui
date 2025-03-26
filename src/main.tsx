ef34cd3670ac5710f099efbf3b31c57513543f9b
sadiq_from_elitebook <sadiqonemail@gmail.com>
2025-03-26 14:47:05 +0530

    setting up nested routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router'
+import { BrowserRouter, Route, Routes } from 'react-router'
 import './index.css'
-import App from './App.tsx'
+import Dashboard from './pages/Dashboard'
+import Layout from './components/Layout'
+import ProtectedRoute from './components/ProtectedRoute'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <Routes>
+        {/* Public routes */}
+        {/* <Route path="/signin" element={<SignIn />} /> */}
+        {/* Protected routes */}
+        <Route element={<ProtectedRoute />}>
+          <Route element={<Layout />}>
+            <Route path="/" element={<Dashboard />} />
+          </Route>
+        </Route>
+      </Routes>
     </BrowserRouter>
   </StrictMode>,
 )
