6459738201690bb33d1090c8f1fa86b3900bd988
sadiq_from_elitebook <sadiqonemail@gmail.com>
2025-03-26 15:09:08 +0530

    dashboard modifided

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,5 +1,14 @@
 function Dashboard() {
-  return <div>Dashboard</div>
+  return (
+    <>
+      <div className="flex flex-1 flex-col gap-4 p-4">
+        DashBoard
+        {Array.from({ length: 24 }).map((_, index) => (
+          <div key={index} className="aspect-video h-12 w-full rounded-lg bg-muted/50" />
+        ))}
+      </div>
+    </>
+  )
 }
 
 export default Dashboard
